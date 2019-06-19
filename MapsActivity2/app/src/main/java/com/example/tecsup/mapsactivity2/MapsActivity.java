30aa82a45cf571d07aa053c07b93021cb4dbd8c9
leodo28 <48073273+leodo28@users.noreply.github.com>
2019-06-18 21:53:47 -0500

    maps y tienda

---
+++ MapsActivity2/app/src/main/java/com/example/tecsup/mapsactivity2/MapsActivity.java
@@ -0,0 +1,68 @@
+package com.example.tecsup.mapsactivity2;
+
+import androidx.fragment.app.FragmentActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-16.429610, -71.519906);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Tecsup"));
+
+
+        CircleOptions c = new CircleOptions();
+        c.center(sydney);
+        c.radius(500);
+        c.strokeColor(Color.RED);
+        c.fillColor(0xAAFFFFFF);
+        mMap.addCircle(c);
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                LatLng nuevo = new LatLng(latLng.latitude, -latLng.longitude);
+                mMap.addMarker(new MarkerOptions().position(nuevo).title("Marker Nuevo"));
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(nuevo));
+            }
+        });
+
+    }
+}
